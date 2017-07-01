64bb43e01b9339c9a48b5bde0900b8b42eff4471
Bill Fienberg <billfienberg@gmail.com>
2017-07-01 16:28:34 -0500

    Add step three (togglable dropdown)

--- src/App.js
+++ src/App.js
@@ -2,18 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isDropdownOpen: false,
+		};
+		this.toggleDropdown = this.toggleDropdown.bind(this);
+	}
+
+	toggleDropdown() {
+		this.setState({ isDropdownOpen: !this.state.isDropdownOpen });
+	}
+
 	render() {
+		const isDropdownOpen = this.state.isDropdownOpen;
+		const iconName = isDropdownOpen
+			? 'keyboard_arrow_down'
+			: 'keyboard_arrow_up';
+		const openDropdownClass = isDropdownOpen ? 'dropdown--isOpen' : '';
+		const dropdownClasses = `dropdown ${openDropdownClass}`;
 		return (
 			<div className="nav">
 				<a href="#" className="logo">Logo</a>
 				<div className="middleNavSection">
 					<div className="nameWithIcon">
 						<a href="#" className="name">Bill Fienberg</a>
-						<button type="button" className="iconButton">
-							<i className="material-icons menuIcon">keyboard_arrow_down</i>
+						<button
+							type="button"
+							className="iconButton"
+							onClick={this.toggleDropdown}
+						>
+							<i className="material-icons menuIcon">{iconName}</i>
 						</button>
 					</div>
-					<div className="dropdown">
+					<div className={dropdownClasses}>
 						<a href="#" className="dropdown-link">Inbox</a>
 						<a href="#" className="dropdown-link">Settings</a>
 						<a href="#" className="dropdown-link">Sign Out</a>
