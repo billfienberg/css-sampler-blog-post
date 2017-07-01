8f120c537464919c686d4c818303613216de14ec
Bill Fienberg <billfienberg@gmail.com>
2017-07-01 16:03:50 -0500

    Add step one (basic header)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="nav">
+				<a href="#" className="logo">Logo</a>
+				<div className="middleNavSection">
+					<div className="nameWithIcon">
+						<a href="#" className="name">Bill Fienberg</a>
+						<button type="button" className="iconButton">
+							<i className="material-icons menuIcon">keyboard_arrow_down</i>
+						</button>
+					</div>
+				</div>
+				<a href="#" className="help">Help</a>
+			</div>
+		);
+	}
 }
 
 export default App;
